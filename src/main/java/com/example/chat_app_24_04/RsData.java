93e341962b71a97429464242d89ae15f9c91c161
Gouiseong <rhdmltjd1324@gmail.com>
2024-04-11 15:56:58 +0900

    ChatMessage 클래스 생성

---
+++ src/main/java/com/example/chat_app_24_04/RsData.java
@@ -0,0 +1,12 @@
+package com.example.chat_app_24_04;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RsData<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+}
